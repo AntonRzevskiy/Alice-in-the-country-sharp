48060af2f47b14978052578000fc9e8ab2c327a6
AntonRzevskiy <31966248+AntonRzevskiy@users.noreply.github.com>
2018-08-04 06:47:19 +0400

    lodash test

--- phrases/index.js
+++ phrases/index.js
@@ -15,7 +15,7 @@ const Phrases = class {
             console.log( _( this.phrases[ phrase ] ) );
             return this.phrases[ phrase ] [0];
         }
-        return 'Не понимаю о чем вы!';
+        return 'Не понимаю о чем вы говорите!';
     }
 
 
